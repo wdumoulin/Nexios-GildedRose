8a68ea35f21d02d61cac1e05f615f52c15d929ea
Wouter Dumoulin <wdu@nexiosit.com>
2022-11-26 09:49:47 +0100

    separated increasing/decreasing quality + cleaned up backstage passes

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -28,45 +28,45 @@ class GildedRose {
   }
 
   private void updateAgedBrie(Item item) {
-    if (item.quality < 50) {
-      if (item.sellIn > 0) {
-        item.quality += 1;
-      } else {
-        item.quality += 2;
-      }
+    if (item.sellIn > 0) {
+      increaseQualityBy(item, 1);
+    } else {
+      increaseQualityBy(item, 2);
     }
     item.sellIn -= 1;
   }
 
   private void updateBackstagePasses(Item item) {
-    if (item.sellIn <= 0) {
-      item.quality = 0;
+    if (item.sellIn > 10) {
+      increaseQualityBy(item, 1);
+    } else if (item.sellIn > 5) {
+      increaseQualityBy(item, 2);
+    } else if (item.sellIn > 0) {
+      increaseQualityBy(item, 3);
     } else {
-      if (item.quality < 50) {
-        item.quality += 1;
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality += 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality += 1;
-          }
-        }
-      }
+      decreaseQualityBy(item, item.quality);
     }
     item.sellIn -= 1;
   }
 
   private void updateRegularItem(Item item) {
-    if (item.quality > 0) {
-      if (item.sellIn > 0) {
-        item.quality -= 1;
-      } else {
-        item.quality -= 2;
-      }
+    if (item.sellIn > 0) {
+      decreaseQualityBy(item, 1);
+    } else {
+      decreaseQualityBy(item, 2);
     }
     item.sellIn -= 1;
+  }
+
+  private void increaseQualityBy(Item item, int quality) {
+    if (item.quality < 50) {
+      item.quality += quality;
+    }
+  }
+
+  private static void decreaseQualityBy(Item item, int quality) {
+    if (item.quality > 0) {
+      item.quality -= quality;
+    }
   }
 }
