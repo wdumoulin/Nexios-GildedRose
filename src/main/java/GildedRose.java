62d6491d8fe56b848548a24efe1474236e32b724
Wouter Dumoulin <wdu@nexiosit.com>
2022-11-26 09:23:37 +0100

    separated logic per item type

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,65 +1,59 @@
 class GildedRose {
 
-	static final String AGED_BRIE = "Aged Brie";
-	static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-	static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+  static final String AGED_BRIE = "Aged Brie";
+  static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+  static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-	Item[] items;
+  Item[] items;
 
-	public GildedRose(Item[] items) {
-		this.items = items;
-	}
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
 
-	public void updateQuality() {
-		for (Item item : items) {
-			if (!AGED_BRIE.equals(item.name) && !BACKSTAGE_PASSES.equals(item.name)) {
-				if (item.quality > 0) {
-					if (!SULFURAS.equals(item.name)) {
-						item.quality = item.quality - 1;
-					}
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
+  public void updateQuality() {
+    for (Item item : items) {
 
-					if (BACKSTAGE_PASSES.equals(item.name)) {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
+      if (SULFURAS.equals(item.name)) {
+        // Do nothing
+      } else if (AGED_BRIE.equals(item.name)) {
+        if (item.quality < 50) {
+          if (item.sellIn > 0) {
+            item.quality = item.quality + 1;
+          } else {
+            item.quality = item.quality + 2;
+          }
+        }
+        item.sellIn = item.sellIn - 1;
+      } else if (BACKSTAGE_PASSES.equals(item.name)) {
+        if (item.sellIn <= 0) {
+          item.quality = 0;
+        } else {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
+              }
+            }
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
+              }
+            }
+          }
+        }
+        item.sellIn = item.sellIn - 1;
+      } else {
+        if (item.quality > 0) {
+          if (item.sellIn > 0) {
+            item.quality = item.quality - 1;
+          } else {
+            item.quality = item.quality - 2;
+          }
+        }
+        item.sellIn = item.sellIn - 1;
+      }
+    }
 
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-					}
-				}
-			}
-
-			if (!SULFURAS.equals(item.name)) {
-				item.sellIn = item.sellIn - 1;
-			}
-
-			if (item.sellIn < 0) {
-				if (!AGED_BRIE.equals(item.name)) {
-					if (!BACKSTAGE_PASSES.equals(item.name)) {
-						if (item.quality > 0) {
-							if (!SULFURAS.equals(item.name)) {
-								item.quality = item.quality - 1;
-							}
-						}
-					} else {
-						item.quality = 0;
-					}
-				} else {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
-			}
-		}
-	}
-
+  }
 }
